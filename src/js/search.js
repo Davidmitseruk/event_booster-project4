94ed2157b8c9e55e3b563b33da358e390fe50061
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-04-18 16:49:14 +0300

    solved some problems

--- src/js/search.js
+++ src/js/search.js
@@ -1,50 +1,48 @@
-"use strict";
-const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=L7VlP0GtWQZMHhIkOqz3A3FcxX8k8AWD';
+'use strict';
+const BASE_URL =
+  'https://app.ticketmaster.com/discovery/v2/events.json?apikey=L7VlP0GtWQZMHhIkOqz3A3FcxX8k8AWD';
 const input = document.querySelector('.header__search');
 const container = document.querySelector('#hero__items');
 
-input.addEventListener('input', (e) => {
-    e.preventDefault();
-    search(input.value);
+input.addEventListener('input', async e => {
+  e.preventDefault();
+  await search(input.value);
 });
 
-async function getEvents(){
-    try {
-      let value = input.value;
-      const r = await fetch(`${BASE_URL}&keyword=${value}`);
-        const data = await r.json();
-        return data._embedded?.events || [];
-    } catch (error){
-        console.error(error);
-        return [];
-    }
-};
+async function getEvents() {
+  try {
+    let value = input.value;
+    const r = await fetch(`${BASE_URL}&keyword=${value}`);
+    const data = await r.json();
+    return data._embedded?.events || [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
 
 async function search(value) {
-    const events = await getEvents();
-    const filtered = events.filter(event =>
-        event.name.includes(value.toLowerCase()) // || event.keyword.toLowerCase().includes(value.toLowerCase()) 
-    );
+  const events = await getEvents();
+  const filtered = events.filter(
+    event => event.name.includes(value.toLowerCase()) // || event.keyword.toLowerCase().includes(value.toLowerCase())
+  );
 
-    if (filtered.length === 0){
-        container.innerHTML = '<h2>There are no such events</h2>';
-        return;
-    }
+  if (filtered.length === 0) {
+    container.innerHTML = '<h2>There are no such events</h2>';
+    return;
+  }
 
-    render(filtered);
+  render(filtered);
 }
 
 function render(events) {
-    const templateSource = `
-      <div class='hero__template'>
+  const templateSource = `
+      <div class='hero__template' >
         <ul class='hero__list'>
           {{#each this}}
-            <li class='hero__item'>
+            <li class='hero__item' data-id="{{id}}">
               <div class='hero__img-wrap'>
-                <picture class='hero__img'>
-                  <source srcset="{{images.0.url}} 1x, {{images.0.url}} 2x" type="image/jpeg" />
                   <img src="{{images.0.url}}" alt="{{name}}" class="hero__img-teg"/>
-                </picture>
               </div>
               <h2 class='hero__name'>{{name}}</h2>
               <p class='hero__date'>{{dates.start.localDate}}</p>
@@ -59,9 +57,9 @@ function render(events) {
         </ul>
       </div>
     `;
-    const template = Handlebars.compile(templateSource);
-    const markUp = template(events);
-    container.innerHTML = markUp;
+  const template = Handlebars.compile(templateSource);
+  const markUp = template(events);
+  container.innerHTML = markUp;
 }
 // async function startApp(){
 //     const allEvents = await getEvents();
