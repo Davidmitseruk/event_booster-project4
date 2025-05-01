748899877534d8eb77f17f2af5194717a8e5cd3b
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2025-05-01 17:35:53 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,7 +13,7 @@ const refs = {
 const template = `<div class='backdrop__poster is-hidden'>
   <div class='modal__poster'>
     <button class="modal__btn-close">
-      <img src="./src/images/close.png" alt="close" />
+      <img src="./close.png" alt="close" />
     </button>
     <img src='{{image}}' alt='Poster Image' class='modal__poster-small' />
     <div class="modal__main-wrap">
@@ -41,9 +41,9 @@ const template = `<div class='backdrop__poster is-hidden'>
             </div>
             <div class='modal__block'>
                 <h2 class='modal__title'>PRICES</h2>
-                <p class='modal__text'> <img class="modal__icon" src="./images/ticket1.png" alt="tickets" > {{priceStandard}}</p>
+                <p class='modal__text'> <img class="modal__icon" src="./ticket1.png" alt="tickets" > {{priceStandard}}</p>
                 <button class="modal__poster-btn">BUY TICKETS</button>
-                <p class='modal__text'> <img class="modal__icon" src="./images/ticket1.png" alt="tickets" > {{priceVIP}}</p>
+                <p class='modal__text'> <img class="modal__icon" src="./ticket1.png" alt="tickets" > {{priceVIP}}</p>
                 <button class="modal__poster-btn">BUY TICKETS</button>
             </div>
             <div class='modal__btn-wrap'>
